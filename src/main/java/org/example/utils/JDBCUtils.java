c94e92b8b5402885b74d7f36fef3d3fa89d76f2d
lstojanovic <lstojanovic5623rn@raf.rs>
2025-05-09 23:56:23 +0200

    end

--- src/main/java/org/example/utils/JDBCUtils.java
+++ src/main/java/org/example/utils/JDBCUtils.java
@@ -27,21 +27,15 @@ public class JDBCUtils {
     }
     public static boolean existsInDatabase(String username, String JMBG){
         try{
-            String query = STR."select* from psihoterapeut where ime = '\{username}' and JMBG = '\{JMBG}'";
-            setUser(query);
-            Statement st = connection.createStatement();
-            ResultSet rs = st.executeQuery(query);
-            return rs.next();
-        }catch (Exception sqlException){
-            Utility.throwMessage("SQL Error", sqlException.getMessage());
-        }
-        return false;
-    }
-
-    private static void setUser(String query) {
-        try{
-            Statement st = connection.createStatement();
-            ResultSet rs = st.executeQuery(query);
+//            String query = STR."select* from psihoterapeut where ime = '\{username}' and JMBG = '\{JMBG}'";
+            String query = "select* from psihoterapeut where ime = ? and JMBG = ?";
+            PreparedStatement st = connection.prepareStatement(query);
+            connection.setAutoCommit(false);
+            st.setString(1, username);
+            st.setString(2, JMBG);
+            //setUser(query);
+//            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery();
             if(rs.next()){
                 int id = rs.getInt(1);
                 String name = rs.getString(2);
@@ -53,17 +47,17 @@ public class JDBCUtils {
                 short psychologist = rs.getShort(8);
                 Psychotherapist.getInstance().setId(id);
                 Utility.updateUserModel(name, lastname, UCIN, DOB, POR, phoneNumber, psychologist);
-//                System.out.println(id);
-//                System.out.println(name);
-//                System.out.println(lastname);
-//                System.out.println(UCIN);
-//                System.out.println(DOB);
-//                System.out.println(psychologist);
-            }else Utility.throwMessage("Error", "Invalid user / no user found");
+                connection.commit();
+                return true;
+            }else{
+                connection.commit();
+                return false;
+            }
 
-        } catch (SQLException e) {
-            Utility.throwMessage("SQL Error", e.getMessage());
+        }catch (Exception sqlException){
+            Utility.throwMessage("SQL Error", sqlException.getMessage());
         }
+        return false;
     }
 
     public static void insertPsychotherapist(String name, String lastname, String UCIN, String DOB, String POR, String phoneNumber, String psychologist){
@@ -82,7 +76,6 @@ public class JDBCUtils {
 
             ps.executeUpdate();
             connection.commit();
-
             Utility.throwMessage("Success", "A new psychotherapist has been added to the database 'New Beginning'");
         }catch (SQLException ex){
             Utility.throwMessage("Error", ex.getMessage());
@@ -139,9 +132,13 @@ public class JDBCUtils {
 //                    "join Prijava p on p.klijent_id = k.klijent_id " +
 //                    "join seansa s on s.seansa_id = p.Seansa_seansa_id " +
 //                    "where k.klijent_id = " + Psychotherapist.getInstance().getId();
-            String query = STR."select distinct k.klijent_id, k.ime, k.prezime, k.datum_rodjenja, k.pol, k.email, k.broj_telefona from klijent k join Prijava p on p.klijent_id = k.klijent_id join seansa s on s.seansa_id = p.Seansa_seansa_id where k.klijent_id = \{Psychotherapist.getInstance().getId()}";
-            Statement st = connection.createStatement();
-            ResultSet rs = st.executeQuery(query);
+//            String query = STR."select distinct k.klijent_id, k.ime, k.prezime, k.datum_rodjenja, k.pol, k.email, k.broj_telefona from klijent k join Prijava p on p.klijent_id = k.klijent_id join seansa s on s.seansa_id = p.Seansa_seansa_id where k.klijent_id = \{Psychotherapist.getInstance().getId()}";
+            String query = "select distinct k.klijent_id, k.ime, k.prezime, k.datum_rodjenja, k.pol, k.email, k.broj_telefona from klijent k join Prijava p on p.klijent_id = k.klijent_id join seansa s on s.seansa_id = p.Seansa_seansa_id where k.klijent_id = ?";
+            PreparedStatement st = connection.prepareStatement(query);
+            connection.setAutoCommit(false);
+            st.setInt(1, Psychotherapist.getInstance().getId());
+//            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery();
             while (rs.next()){
                 int id = rs.getInt(1);
                 String name = rs.getString(2);
@@ -152,6 +149,7 @@ public class JDBCUtils {
                 String phoneNumber = rs.getString(7);
                 NewClientApplicationPanel.addClient(new Object[]{id, name, lastname, DOB, gender, email, phoneNumber});
             }
+            connection.commit();
         }catch (Exception ex) {
             Utility.throwMessage("SQL Error", ex.getMessage());
         }
@@ -163,9 +161,12 @@ public class JDBCUtils {
             String query = "select s.*, c.trenutna_cena from seansa s " +
                     "join psihoterapeut p on p.psihoterapeut_id=s.Psihoterapeut_psihoterapeut_id " +
                     "left join cena c on c.cena_id = s.Cena_cena_id " +
-                    "where s.dan_vreme < NOW() and p.psihoterapeut_id = " + Psychotherapist.getInstance().getId();
-            Statement st = connection.createStatement();
-            ResultSet rs = st.executeQuery(query);
+                    "where s.dan_vreme < NOW() and p.psihoterapeut_id = ?";
+            PreparedStatement st = connection.prepareStatement(query);
+            connection.setAutoCommit(false);
+            st.setInt(1, Psychotherapist.getInstance().getId());
+//            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery();
             while (rs.next()){
                 int id = rs.getInt(1);
                 String dateTime = rs.getTimestamp(2).toString();
@@ -174,6 +175,7 @@ public class JDBCUtils {
                 float price = rs.getFloat("trenutna_cena");
                 CompletedSessionsPanel.addSession(new Object[]{id, dateTime, duration, notes, price});
             }
+            connection.commit();
         }catch (Exception ex) {
 //            ex.printStackTrace();
             Utility.throwMessage("SQL Error", ex.getMessage());
@@ -186,9 +188,12 @@ public class JDBCUtils {
             String query = "select s.*, c.trenutna_cena from seansa s " +
                     "join psihoterapeut p on p.psihoterapeut_id=s.Psihoterapeut_psihoterapeut_id " +
                     "left join cena c on c.cena_id = s.Cena_cena_id " +
-                    "where s.dan_vreme >= NOW() and p.psihoterapeut_id = " + Psychotherapist.getInstance().getId();
-            Statement st = connection.createStatement();
-            ResultSet rs = st.executeQuery(query);
+                    "where s.dan_vreme >= NOW() and p.psihoterapeut_id = ?";
+//            Statement st = connection.createStatement();
+            PreparedStatement st = connection.prepareStatement(query);
+            connection.setAutoCommit(false);
+            st.setInt(1, Psychotherapist.getInstance().getId());
+            ResultSet rs = st.executeQuery();
             while (rs.next()){
                 int id = rs.getInt(1);
                 String dateTime = rs.getTimestamp(2).toString();
@@ -199,6 +204,7 @@ public class JDBCUtils {
                 float price = rs.getFloat("trenutna_cena");
                 UpcomingSessionsPanel.addSession(new Object[]{id, dateTime, duration, notes, price});
             }
+            connection.commit();
         }catch (Exception ex) {
 //            ex.printStackTrace();
             Utility.throwMessage("SQL Error", ex.getMessage());
@@ -207,11 +213,17 @@ public class JDBCUtils {
 
     public static String getNotes() {
         try {
-            String query = STR."select s.beleske from seansa s where seansa_id = \{NotesAndTestsForm.getSeansa_id()}";
-            Statement st = connection.createStatement();
-            ResultSet rs = st.executeQuery(query);
-            if(rs.next())
+//            String query = STR."select s.beleske from seansa s where seansa_id = \{NotesAndTestsForm.getSeansa_id()}";
+            String query = "select s.beleske from seansa s where seansa_id = ?";
+            PreparedStatement st = connection.prepareStatement(query);
+            connection.setAutoCommit(false);
+            st.setInt(1, NotesAndTestsForm.getSeansa_id());
+//            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery();
+            if(rs.next()) {
+                connection.commit();
                 return rs.getString("beleske");
+            }
             else Utility.throwMessage("Error", "No results found for notes with id = " + NotesAndTestsForm.getSeansa_id());
         }catch (Exception ex){
             Utility.throwMessage("Error", ex.getMessage());
@@ -224,14 +236,18 @@ public class JDBCUtils {
         try{
             String query = "select pt.naziv, rezultat from psiholoski_test pt " +
                     "left join izrada_testa it on it.Psiholoski_test_psiholoski_test_id = pt.psiholoski_test_id " +
-                    "where it.Seansa_seansa_id = " + NotesAndTestsForm.getSeansa_id();
-            Statement st = connection.createStatement();
-            ResultSet rs = st.executeQuery(query);
+                    "where it.Seansa_seansa_id = ?";
+            PreparedStatement st = connection.prepareStatement(query);
+            connection.setAutoCommit(false);
+            st.setInt(1, NotesAndTestsForm.getSeansa_id());
+//            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery();
             while (rs.next()){
                 String name = rs.getString("naziv");
                 int result = rs.getInt("rezultat");
                 NotesAndTestsForm.addTest(new Object[]{name, result});
             }
+            connection.commit();
         }catch (Exception ex){
             Utility.throwMessage("SQL Error", ex.getMessage());
         }
@@ -241,16 +257,19 @@ public class JDBCUtils {
         try {
             String query = "select op.datum_objavljivanja, op.kome_je_objavljeno, vo.naziv from objavljivanje_podataka op " +
                     "left join vrsta_objavljivanja vo on op.Vrsta_objavljivanja_vrsta_objavljivanja_id = vo.vrsta_objavljivanja_id " +
-                    "where op.Seansa_seansa_id = " + NotesAndTestsForm.getSeansa_id();
-            Statement st = connection.createStatement();
-            ResultSet rs = st.executeQuery(query);
+                    "where op.Seansa_seansa_id = ?";
+            PreparedStatement st = connection.prepareStatement(query);
+            connection.setAutoCommit(false);
+            st.setInt(1, NotesAndTestsForm.getSeansa_id());
+//            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery();
             while (rs.next()){
                 Date dateofPublish = rs.getDate("datum_objavljivanja");
                 String whoGotIt = rs.getString("kome_je_objavljeno");
                 String typeofPublish = rs.getString("naziv");
                 NotesAndTestsForm.addPublishData(new Object[]{dateofPublish, whoGotIt, typeofPublish});
             }
-
+            connection.commit();
         }catch (Exception ex){
             Utility.throwMessage("SQL Error", ex.getMessage());
         }
@@ -261,9 +280,12 @@ public class JDBCUtils {
             String query = "select s.*, c.trenutna_cena from seansa s " +
                 "join psihoterapeut p on p.psihoterapeut_id=s.Psihoterapeut_psihoterapeut_id " +
                 "left join cena c on c.cena_id = s.Cena_cena_id " +
-                "where p.psihoterapeut_id = " + Psychotherapist.getInstance().getId();
-            Statement st = connection.createStatement();
-            ResultSet rs = st.executeQuery(query);
+                "where p.psihoterapeut_id = ?";
+            PreparedStatement st = connection.prepareStatement(query);
+            connection.setAutoCommit(false);
+            st.setInt(1, Psychotherapist.getInstance().getId());
+//            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery();
             while (rs.next()){
                 int id = rs.getInt(1);
                 String dateTime = rs.getTimestamp(2).toString();
@@ -274,6 +296,7 @@ public class JDBCUtils {
                 float price = rs.getFloat("trenutna_cena");
                 DataPublishPanel.addSession(new Object[]{id, dateTime, duration, notes, price});
             }
+            connection.commit();
         }catch (Exception ex) {
 //            ex.printStackTrace();
             Utility.throwMessage("SQL Error", ex.getMessage());
@@ -285,12 +308,14 @@ public class JDBCUtils {
             String query = "call insert_objavljivanje_podataka (?, ?, ?, ?)";
             int type_id = returnIdFromDatabase(typeofPublish);
             PreparedStatement st = connection.prepareStatement(query);
+            connection.setAutoCommit(false);
             java.sql.Date date = java.sql.Date.valueOf(dateofPublish);
             st.setDate(1, date);
             st.setString(2, publishedTo);
             st.setInt(3, type_id);
             st.setInt(4, PublishDataForm.getSession_id());
             int affectedRows = st.executeUpdate();
+            connection.commit();
             if(affectedRows > 0)
                 Utility.throwMessage("Success", "Data Publish table updated successfully");
             else Utility.throwMessage("Error", "Data Publish table could not be updated");
@@ -301,9 +326,12 @@ public class JDBCUtils {
 
     private static int returnIdFromDatabase(String type) {
         try{
-            String query = "select vrsta_objavljivanja_id from vrsta_objavljivanja where naziv = '" + type + "'";
-            Statement st = connection.createStatement();
-            ResultSet rs = st.executeQuery(query);
+            String query = "select vrsta_objavljivanja_id from vrsta_objavljivanja where naziv = ?";
+            PreparedStatement st = connection.prepareStatement(query);
+            connection.setAutoCommit(false);
+            st.setString(1, type);
+//            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery();
             int id = 0;
             if(rs.next())
                 id = rs.getInt("vrsta_objavljivanja_id");
@@ -315,6 +343,7 @@ public class JDBCUtils {
                     id = newResultSet.getInt("IDS") + 1;
             }
             insertNewTypeOfPublish(id, type);
+            connection.commit();
             return id;
         }catch (Exception ex){
             Utility.throwMessage("SQL Error", ex.getMessage());
@@ -326,12 +355,14 @@ public class JDBCUtils {
         try {
             String query = "call insert_vrsta_objavljivanja (?, ?)";
             PreparedStatement st = connection.prepareStatement(query);
+            connection.setAutoCommit(false);
             st.setInt(1, id);
             st.setString(2, type);
             int affectedRows = st.executeUpdate();
             if(affectedRows > 0)
                 Utility.throwMessage("Success", "Type of publish table updated successfully");
             else Utility.throwMessage("Error", "Type of publish table could not be updated");
+            connection.commit();
         }catch (Exception ex){
             Utility.throwMessage("SQL Error", ex.getMessage());
         }
@@ -339,11 +370,11 @@ public class JDBCUtils {
 
     public static void insertIntoTablePayments() {
         try{
-            String query = "call new_proc2("+Psychotherapist.getInstance().getId()+")";
-//            PreparedStatement st = connection.prepareStatement(query);
-//            st.setInt(1, Psychotherapist.getInstance().getId());
-            Statement st = connection.createStatement();
-            ResultSet rs = st.executeQuery(query);
+            String query = "call new_proc2(?)";
+            PreparedStatement st = connection.prepareStatement(query);
+            st.setInt(1, Psychotherapist.getInstance().getId());
+//            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery();
             while(rs.next()){
                 String name = rs.getString("ime");
                 String phoneNumber = rs.getString("broj_telefona");
